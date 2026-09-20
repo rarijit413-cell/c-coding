4f3bef6f7140f6e36a83804cf0eb34cf6c66bf8e
rarijit413-cell <arijitbhattacharya2002@gmail.com>
2026-09-20 23:30:22 +0530

    bi

---
+++ search/binarySearchR.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int BinarySearch(int arr[],int x,int low,int high){
+    if(low<=high){
+        int mid =low+(high-low)/2;
+        if(x==arr[mid])
+            return mid;
+        if(x>arr[mid])
+            return BinarySearch(arr,x,mid+1,high);
+        return BinarySearch(arr,x,low,mid-1);
+    }
+    return -1;
+}
+int main(){
+    int arr[]={10,15,20,25,30,35,40,45,50,55,60};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    printf("Enter value to search: ");
+    int x;
+    scanf("%d",&x);
+    int result=BinarySearch(arr,x,0,n-1);
+    if(result==-1)
+        printf("Not Found!");
+    else
+        printf("Found at %d index",result);
+}
